0fd522194f095946da09bad88c83377b5e10f3c9
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2024-04-11 11:58:23 +0200

    Uses C++

--- devices/pellet-dispenser/firmware/src/main.cpp
+++ devices/pellet-dispenser/firmware/src/main.cpp
@@ -1,15 +1,15 @@
-#include <stdio.h>
 #include "pico/stdlib.h"
+#include <stdio.h>
 
 int main() {
 	setup_default_uart();
 
 	int n = 0;
-	printf("\n");
+	printf("\033[2J Welcome to pellet dispenser debug output.\n\n");
 	while (true) {
 		n++;
 
-		printf("\033[ATime is %d\n", n);
+		printf("\033[Auptime: %ds\n", n);
 		sleep_ms(1000);
 	}
 	return 0;
