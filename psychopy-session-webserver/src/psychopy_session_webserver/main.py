df062dbd090c975fbefeb82bd7c16cae9fbfa0fd
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2024-06-19 11:32:30 +0200

    Adds script options

--- psychopy-session-webserver/src/psychopy_session_webserver/main.py
+++ psychopy-session-webserver/src/psychopy_session_webserver/main.py
@@ -2,6 +2,18 @@ import argparse
 
 
 def parse_options(args=None):
+
+    class OverrideDefaultAppend(argparse.Action):
+        def __init__(self, option_strings, dest, **kwargs):
+            self._set = False
+            super().__init__(option_strings, dest, **kwargs)
+
+        def __call__(self, parser, namespace, values, option_string=None):
+            if self._set is False:
+                self._set = True
+                setattr(namespace, self.dest, [])
+            getattr(namespace, self.dest).append(values)
+
     parser = argparse.ArgumentParser(
         prog="psychopy-session-webserver",
         description="Allows PsychoPy session to be run via a REST API",
@@ -10,9 +22,9 @@ def parse_options(args=None):
     parser.add_argument(
         "-l",
         "--listen",
-        nargs="*",
         help="address to listen to, defaults to a set of private subnet address",
         default=["192.168.0.0/16", "10.0.0.0/8", "100.64.0.0/10"],
+        action=OverrideDefaultAppend,
     )
 
     parser.add_argument(
@@ -22,12 +34,11 @@ def parse_options(args=None):
     parser.add_argument(
         "-d",
         "--data-dir",
-        help="directory to store the session data, default to <session-dir>/data",
+        help="directory to store the session data, default to <session_dir>/data",
     )
     parser.add_argument(
-        "session-dir",
-        nargs=1,
+        "session_dir",
         help="directory containing all .psyexp file available in the session",
     )
 
-    return parser.parse_args(args)
+    return vars(parser.parse_args(args))
--- psychopy-session-webserver/tests/test_scripts_arguments.py
+++ psychopy-session-webserver/tests/test_scripts_arguments.py
@@ -9,6 +9,26 @@ class ScriptArgumentTest(unittest.TestCase):
         with self.assertRaises(SystemExit) as e:
             parse_options([])
 
-    def test_directory(self):
+    def test_default(self):
         opts = parse_options(["./foo"])
-        self.assertEqual(opts.session_dir, "./foo")
+        self.assertDictEqual(
+            {
+                "session_dir": "./foo",
+                "listen": ["192.168.0.0/16", "10.0.0.0/8", "100.64.0.0/10"],
+                "port": 5000,
+                "data_dir": None,
+            },
+            opts,
+        )
+
+    def test_listen_overrrides_default(self):
+        opts = parse_options(["-l", "0.0.0.0", "-l", "::", "./foo"])
+        self.assertDictEqual(
+            {
+                "session_dir": "./foo",
+                "listen": ["0.0.0.0", "::"],
+                "port": 5000,
+                "data_dir": None,
+            },
+            opts,
+        )
