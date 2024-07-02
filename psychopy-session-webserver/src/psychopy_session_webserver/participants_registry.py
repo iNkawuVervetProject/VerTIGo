bb1263ca773369ff219fab34a174ba6938f0b22d
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2024-07-02 09:52:27 +0200

    Makes ParticipantRegistry somewhat robust to model change
    A breaking change in the Participant model will simply ignore previous data.

--- psychopy-session-webserver/src/psychopy_session_webserver/participants_registry.py
+++ psychopy-session-webserver/src/psychopy_session_webserver/participants_registry.py
@@ -2,6 +2,8 @@ from glob import glob
 from pathlib import Path
 
 from numpy import true_divide
+from pydantic import ValidationError
+from structlog import get_logger
 from psychopy_session_webserver.types import Participant
 from psychopy_session_webserver.update_broadcaster import UpdateBroadcaster
 from pydantic_core import to_json, from_json
@@ -13,6 +15,7 @@ from xdg import BaseDirectory
 class ParticipantRegistry:
 
     def __init__(self, updates: UpdateBroadcaster, dataDir=None):
+        self._logger = get_logger().bind(module="ParticipantRegistry")
         self._updates = updates
         self._participants = dict[str, Participant]({})
         self._load_from_xdg()
@@ -41,23 +44,31 @@ class ParticipantRegistry:
     def _load_from_xdg(self):
         if ParticipantRegistry._filepath.exists() == False:
             return
-
+        self._logger.info(
+            "opening persistent file", path=str(ParticipantRegistry._filepath)
+        )
         with open(ParticipantRegistry._filepath, "r") as f:
-            self._participants.update(
-                {k: Participant(**v) for k, v in from_json(f.read()).items()}
-            )
+            for k, v in from_json(f.read()).items():
+                try:
+                    self._participants[k] = Participant(**v)
+                except ValidationError as e:
+                    self._logger.warn("invalid data", key=k, **v)
 
     def _save_to_xdg(self):
         with open(ParticipantRegistry._filepath, "wb") as f:
             f.write(to_json(self._participants, indent=2))
 
     def _update_from_data(self, dataDir):
-        participants = {}
+        nextSessionBounds = {}
         for f in glob(str(Path(dataDir or "data").joinpath("**/*.psydat"))):
+            # this formula below is only valid when Participant.name validation forbids
+            # the use of '_' in the name, this is the case as it as a pattern
+            # r'^[a-zA-Z0-9\-]+$' set.
             name = Path(f).stem.split("_")[0]
-            participants[name] = participants.get(name, 1) + 1
 
-        for name, nextSession in participants.items():
+            nextSessionBounds[name] = nextSessionBounds.get(name, 1) + 1
+
+        for name, nextSession in nextSessionBounds.items():
             if name not in self._participants:
                 self._participants[name] = Participant(name=name, nextSession=1)
             self._participants[name].update(nextSession)
