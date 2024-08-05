d064800ebb67199d3e8a565efcd15bbb79dbc447
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2024-08-05 11:49:53 +0200

    Basic sphinx configuration

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -6,22 +6,21 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'VerTIGo'
-copyright = '2024, Alexandre Tuleu'
-author = 'Alexandre Tuleu'
+project = "VerTIGo"
+copyright = "2024, Alexandre Tuleu"
+author = "Alexandre Tuleu"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["sphinx.ext.githubpages", "sphinx.ext.duration"]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "nature"
+html_static_path = ["_static"]
