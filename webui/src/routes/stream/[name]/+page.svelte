1b392f2730133e890fa7c3fb970d52e141489d47
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2024-08-22 12:08:49 +0200

    Computes the right url from the current origin.

--- webui/src/routes/stream/[name]/+page.svelte
+++ webui/src/routes/stream/[name]/+page.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-	import { dev } from '$app/environment';
 	import { page } from '$app/stores';
 	import WhepPlayer from '$lib/whep_player.svelte';
 
-	const origin = dev ? 'http://localhost:8889' : '/stream';
+	$: url = `${$page.url.protocol}://${$page.url.hostname}:8889/${$page.params.name}`;
 </script>
 
-<WhepPlayer url={origin + '/' + $page.params.name} />
+<WhepPlayer {url} />
